281916b774c577cfe6679a8a82caab5651f8f200
Abdullah Nurbani Adam <@nurbani.adam>
2024-03-07 00:20:06 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use APP\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+
+            $usertype = Auth()->user()->usertype;
+
+            if ($usertype == 'user') {
+                return view('dashboard');
+            } else if ($usertype == 'admin') {
+                return view('admin.adminHome');
+            } else {
+                return redirect()->back();
+            }
+        }
+    }
+}
